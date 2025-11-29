0f1fc22a837251d0571d7efb4077f954984630b9
GioMjds <giomjds@gmail.com>
2025-11-29 10:40:17 +0800

    fix: settings.py

--- server/hotel_backend/hotel_backend/settings.py
+++ server/hotel_backend/hotel_backend/settings.py
@@ -224,6 +224,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
